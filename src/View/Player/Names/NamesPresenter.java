bcda729995baf7930496d6d12de60c22c3c301e1
st0n1e <cis.verbruggen@student.kdg.be>
2023-03-15 14:34:16 +0100

    Implemented empty name exception

--- src/View/Player/Names/NamesPresenter.java
+++ src/View/Player/Names/NamesPresenter.java
@@ -1,12 +1,14 @@
 package View.Player.Names;
 
 import Model.Player;
+import Model.RegenwormenException;
 import Model.RegenwormenModel;
 import View.Regenwormen.RegenwormenPresenter;
 import View.Regenwormen.RegenwormenView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
@@ -25,20 +27,28 @@ public class NamesPresenter {
         view.getConfirm().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                List<Player> players = new ArrayList<>();
-                for (TextField playerName : view.getPlayerNames()) {
-                    if (playerName.getText().isEmpty()) {
-                        //Throw exception
+                try {
+                    List<Player> players = new ArrayList<>();
+                    for (TextField playerName : view.getPlayerNames()) {
+                        if (playerName.getText().isEmpty()) {
+                            throw new RegenwormenException("Gelieve alle namen in te vullen!!");
+                        }
+                        players.add(new Player(playerName.getText()));
+                        model.setPlayers(players);
                     }
-                    players.add(new Player(playerName.getText()));
-                    model.setPlayers(players);
+                    RegenwormenView namesView = new RegenwormenView();
+                    RegenwormenPresenter presentor = new RegenwormenPresenter(model, namesView);
+                    Scene scene = view.getScene();
+                    scene.setRoot(namesView);
+                } catch (RegenwormenException ex) {
+                    alert(ex.getMessage());
                 }
-                RegenwormenView namesView = new RegenwormenView();
-                RegenwormenPresenter presentor = new RegenwormenPresenter(model, namesView);
-                Scene scene = view.getScene();
-                scene.setRoot(namesView);
             }
         });
+    }
+    private void alert(String message) {
+        Alert ex = new Alert(Alert.AlertType.ERROR, message);
+        ex.showAndWait();
     }
 
 }
